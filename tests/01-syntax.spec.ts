3b59507b97bac685940738f3783dd07802e5b00c
Thỏa Kim <thoadinh000@gmail.com>
2025-05-11 17:36:02 +0700

    update

--- tests/01-syntax.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-test.describe('Suite 1', async() => {
-    test ('test 1', async ({page})=> {
-        await test.step('Step 1: Truy cap website', async() => {
-    
-        })
-    });
-});
---
+++ tests/login.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+ const HomepageURL = "https://localhost:44390/";
+
+test('getlogin', async({page})=>{
+  await page.goto(HomepageURL);
+  await page.getByRole("link", {name: ' Tài khoản '}).hover();
+  const hoverText = page.locator("//ul[@class = 'account_selection']");
+  await expect(hoverText).toBeVisible();
+  await page.locator("//a[@href = '/account/login']").click();
+});
